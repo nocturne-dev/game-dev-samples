c15097552849acc1036cbe90b4fd25bf2420aa2e
Frankii Tang <ftang14@gmail.com>
2022-06-01 11:48:06 -0700

    Animation Control 3D
    Added 'Animation Control 3D' project

---
+++ animation-control-3d/Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Animator anim;
+    public Rigidbody rbody;
+
+    private bool run;
+    private float inputH;
+    private float inputV;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        run = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("1"))
+        {
+            //animation WAIT01, at base layer, playing at the start of the animation
+            anim.Play("WAIT01", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            //animation WAIT02, at base layer, playing at the start of the animation
+            anim.Play("WAIT02", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            //animation WAIT03, at base layer, playing at the start of the animation
+            anim.Play("WAIT03", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            //animation WAIT04, at base layer, playing at the start of the animation
+            anim.Play("WAIT04", -1, 0f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            int n = Random.Range(0, 2);
+
+            if(n == 0)
+            {
+                anim.Play("DAMAGED00", -1, 0f);
+            }
+
+            else
+            {
+                anim.Play("DAMAGED01", -1, 0f);
+            }
+            
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("Jump", true);
+        }
+        else
+        {
+            anim.SetBool("Jump", false);
+        }
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetBool("Run", run);
+        anim.SetFloat("InputH", inputH);
+        anim.SetFloat("InputV", inputV);
+
+        float moveX = inputH * 20f * Time.deltaTime;
+        float moveZ = inputV * 50f * Time.deltaTime;
+
+        if(moveZ <= 0f)
+        {
+            moveX = 0f;
+        }
+        else if(run)
+        {
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
+
+        rbody.velocity = new Vector3(moveX, 0f, moveZ);
+    }
+}
---
+++ animation-control-3d/Assets/Scripts/Respawn.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour {
+
+    public static int levelN = 0;
+
+    private Vector3 startPos;
+    private Quaternion startRot;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        startRot = transform.rotation;
+	}
+
+    void NextLevel()
+    {
+        levelN++;
+
+        if(levelN > 1)
+        {
+            levelN = 0;
+        }
+
+        SceneManager.LoadScene(levelN);
+    }
+
+    //detect collision with trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Death")
+        {
+            transform.position = startPos;
+            transform.rotation = startRot;
+            GetComponent<Animator>().Play("LOSE00", -1, 0f);
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
+        }
+
+        else if(other.tag == "Checkpoint")
+        {
+            startPos = other.transform.position;
+            startRot = other.transform.rotation;
+
+            Destroy(other.gameObject);
+        }
+
+        else if(other.tag == "Goal")
+        {
+            Destroy(other.gameObject);
+            GetComponent<Animator>().Play("WIN00", -1, 0f);
+
+            Invoke("NextLevel", 2.0f);
+        }
+    }
+}
