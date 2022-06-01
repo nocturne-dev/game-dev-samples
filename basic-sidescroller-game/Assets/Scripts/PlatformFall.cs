68358005f945a450eda94497282e81301e093d72
Frankii Tang <ftang14@gmail.com>
2022-06-01 11:31:18 -0700

    Basic Sidescroller Game
    Added 'Basic Sidescroller Game' project

---
+++ basic-sidescroller-game/Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+    public float fallDelay = 1f;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) ;
+        Invoke("Fall", fallDelay);
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+}
